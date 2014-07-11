ab560de1998c80c64f4fd6c8b3e1d59b695f3f37
Robert Kim <rkty13@gmail.com>
2014-07-11 16:27:19 -0400

    please work

--- src/main/java/com/youtube/randomize/Runner.java
+++ src/main/java/com/youtube/randomize/Runner.java
@@ -18,7 +18,7 @@ public class Runner extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getWriter().println("Made by Robert Kim!\n");
+		resp.getWriter().println("Made by Robert Kim\n");
 		resp.getWriter().println("Version 1.0.1\n");
 		new Search(resp);
 	}
